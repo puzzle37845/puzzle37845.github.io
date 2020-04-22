00226b30c203af6bfc5a690209a4b42046df4dfb
kpwebsite2019 <50060086+kpwebsite2019@users.noreply.github.com>
2020-04-22 19:22:32 -0400

    testing

---
+++ keys.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; //importing needed utilities
+import java.util.Random;
+import java.lang.Math;
+import java.lang.String;
+
+public class Keys {
+public static void Hello() {
+System.out.println("hello");
+}
+
+public static void main(String[] args) {
+    System.out.print("hello");
+}
+}
+

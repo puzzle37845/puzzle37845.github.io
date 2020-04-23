f20b2d9b969552b0767b395b08cfd1f6e5512581
kpwebsite2019 <50060086+kpwebsite2019@users.noreply.github.com>
2020-04-23 12:17:38 -0400

    creating keys

--- key1.js
+++ key1.js
@@ -1,6 +1,6 @@
-function CheckKey1(inputtxt) {
-    var key1 = the;
-    if (inputtxt.value.match(inputtxt)) {
+function CheckKey1() {
+    var key1 = "the";
+    if (inputtxt.value.match(key1attempt)) {
         alert("correct");
     }
 

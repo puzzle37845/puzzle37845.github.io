3e87f5e8bbff45a350d723ec851fc40b78dc42c3
kpwebsite2019 <50060086+kpwebsite2019@users.noreply.github.com>
2020-04-22 22:17:45 -0400

    applet testing

--- keys.java
+++ keys.java
@@ -9,18 +9,6 @@ import java.awt.Graphics;
 public class Keys extends Applet {
    public void paint(Graphics g) {
    g.drawString("Welcome to Java Applet",40,20);
-    //   String key1 = "";
-//       String key2 = "";
-//       String key3 = "";
-//       System.out.println("hello");
-//       Scanner input1 = new Scanner(System.in);
-// 
-//      String guess1 = input1.nextLine();
-//       if (guess1.equals("the")) {
-//          System.out.println("Congrats");
-//       } else {
-//          System.out.println("no");
-//       }
    }
 
 
